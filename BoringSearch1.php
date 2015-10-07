66d831dccc812a2d09ad7578cccfa1459f4657c4
daniMatar <matar.dani@gmail.com>
2015-10-07 11:12:55 -0300

    Initial commitLa

---
+++ BoringSearch1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Step 2 </title>
+    <style>
+        table, th, tr, td { border: solid 2px black;}
+    </style>
+</head>
+<body>
+
+<form action="BoringSearch2.php" method="POST">
+    <input id="search" type="text" placeholder="Search here">
+    <input id="submit" type="submit" value="Search">
+</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
---
+++ Connect.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: inet2005
+ * Date: 10/5/15
+ * Time: 11:10 AM
+ */
+
+
+function getDbConnection()
+{
+    $conn = mysqli_connect("localhost", "root", "inet2005", "sakila");
+    if(!$conn)
+    {
+        die("Unable to connect to database: " . mysqli_connect_error());
+    }
+
+    return $conn;
+}
+
+
+
+
+
+
+
+
+
+
