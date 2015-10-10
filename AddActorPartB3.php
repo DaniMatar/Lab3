d9e684a90fdd713db2953b46c84f25c2d7f35395
daniMatar <matar.dani@gmail.com>
2015-10-10 17:50:30 -0300

    5thSubmit

--- AddActorPartB3.php
+++ AddActorPartB3.php
@@ -18,12 +18,12 @@
 ?>
 <table border="1">
     <thead>
-    <th>Title</th>
-    <th>Description</th>
+    <th>First Name</th>
+    <th>Last Name</th>
     </thead>
     <?php
 
-    $Display = "SELECT first_name,last_name FROM actor ORDER BY actor_id LIMIT 10";
+    $Display = "SELECT first_name,last_name FROM actor ORDER BY actor_id DESC LIMIT 10";
     $result2 = mysqli_query($conn, $Display);
 
     if (!$result2) {
@@ -33,8 +33,9 @@
     while ($row = mysqli_fetch_assoc($result2)):
         ?>
         <tr>
+            <td><?php echo $row['actor_id'] ?></td>
             <td><?php echo $row['first_name'] ?></td>
-            <td><?php echo $row['last_name'] ?></td>
+            <td><?php echo $row['last_name'] ?></td>>
         </tr>
 
     <?php
--- DeleteActor.php
+++ DeleteActor.php
@@ -4,6 +4,12 @@
     <title>Add new actors</title>
 </head>
 <body>
+<form id="deleteActor" name="deleteActor" method="post" action="DeleteActor.php">
+    <p><label>Actor Id: <input type="text" name="actorId" id="actorId" /></label></p>
+    <p><input type="submit" id="submit" value="delete" /></p>
+</form>
+
+
 
 <?php
 if(!empty($_POST['actorId']))
@@ -25,14 +31,16 @@ if(!$result)
 }
 ?>
 
-<table>
+<table border = "1">
     <thead>
+    <th>ID</th>
     <th>First Name</th>
     <th>Last Name</th>
+
     </thead>
     <?php
 
-    $Display = "SELECT first_name,last_name, actor_id  FROM actor";
+    $Display = "SELECT first_name,last_name, actor_id FROM actor ORDER BY actor.actor_id DESC LIMIT 10";
     $result2= mysqli_query($conn, $Display);
 
     if(!$result2)
@@ -43,7 +51,10 @@ if(!$result)
     while($row = mysqli_fetch_assoc($result2)):
         ?>
         <tr>
+            <td><?php echo $row['actor_id'] ?></td>
             <td><?php echo $row['first_name'] ?></td>
+            <td><?php echo $row['last_name'] ?></td>
+
 
         </tr>
 
@@ -58,68 +69,6 @@ if(!$result)
     ?>
     </tbody>
 </table>
-</body>
-</html>
-
-
-======================================================================
-
-
-
-
-
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Delete actors</title>
-</head>
-<body>
-<form id="deleteActor" name="deleteActor" method="post" action="DeleteActor.php">
-    <p><label>Actor Id: <input type="text" name="actorId" id="actorId" /></label></p>
-    <p><input type="submit" id="submit" value="delete" /></p>
-</form>
-<?php
-if(!empty($_POST['actorId']))
-{
-    $conn = mysqli_connect("localhost", "root", "inet2005", "sakila");
-    if(!$conn)
-    {
-        die("Unable to connect to database: " . mysqli_connect_error());
-    }
-
-    $sql = "DELETE FROM actor WHERE actor_id = ";
-    $sql .= $_POST['actorId'];
-    $sql .= ";";
-
-    $result = mysqli_query($conn, $sql);
-    if(!$result)
-    {
-        die("Unable to delete record: " . mysqli_error($conn));
-    }
-    else
-    {
-
-
-        while($row = $result.mysqli_affected_rows($conn)):
-            ?>
-            <tr>
-                <td><?php echo $row['title'] ?></td>
-
-            </tr>
-
-        <?php
-        endwhile;
-        echo "Affected rows:  ". $result.mysqli_affected_rows($conn);
-    }
-
-
-
-    mysqli_close($conn);
-}
-
-?>
 </body>
 </html>
 
